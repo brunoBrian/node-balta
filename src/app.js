b6b0ede1dc6c585556273b921b76ce983b58526a
Bruno Brian <bruno.brian@b2wdigital>
2019-07-10 15:56:02 -0300

    feat: criando crud

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 // Arquivo de rota
 const router = express.Router();
+
+// Converte o conteudo para json 
+app.use(bodyParser.json());
+// Codifica as urls
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Criando uma rota
 const route = router.get('/', (req, res, next) => {
@@ -12,6 +18,29 @@ const route = router.get('/', (req, res, next) => {
   });
 });
 
+// Criando um registro na api
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+// Atualizando um registro na api
+const put = router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  res.status(200).send({ 
+    id: id,
+    item: req.body
+  });
+});
+
+// Deletando um registro na api
+const del = router.delete('/:id', (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
